62b37cdc3720d8b27e187d2d48812a50d3f6ccde
Geoffrey Goodman <ggoodman@gmail.com>
2016-08-16 12:33:56 -0400

    Build response in the compiler

--- index.js
+++ index.js
@@ -11,18 +11,52 @@ module.exports = {
 function authorize_and_process_body(options, cb) {
     options.nodejsCompiler(options.script, function (error, func) {
         if (error) return cb(error);
-        return cb(null, function (ctx, cb) {
+        return cb(null, function (ctx, req, res) {
             if (is_authorized(ctx, cb)) {
                 if (typeof ctx.body !== 'object')
                     return cb(Errors.badRequest());
                 if (typeof ctx.body.client !== 'object')
                     return cb(Errors.badRequest());
-                if (!Array.isArray(ctx.body.scope))
+                if (ctx.body.scope && !Array.isArray(ctx.body.scope))
                     return cb(Errors.badRequest());
                 if (typeof ctx.body.audience !== 'string')
                     return cb(Errors.badRequest());
 
-                return func(ctx.body.client, ctx.body.scope, ctx.body.audience, cb);
+                return func(ctx.body.client, ctx.body.scope, ctx.body.audience, buildResponse);
+            }
+            
+            
+            function buildResponse(error, data) {
+                var response = {
+                    statusCode: 200,
+                    headers: {
+                        'Content-Type': 'application/javascript',
+                    },
+                    data,
+                }
+                
+                if (error) {
+                    response.statusCode = error.statusCode || 500;
+                    response.data = {
+                        message: error.message || 'Unkown error',
+                        code: error.code || 'unknown_error',
+                        statusCode: error.statusCode || 500,
+                        stack: error.stack,
+                    };
+                }
+                
+                return respond(response);
+            }
+            
+            function respond(response) {
+                try {
+                    const body = JSON.stringify(response.data);
+                    
+                    res.writeHead(response.statusCode, response.headers);
+                    res.end(body);
+                } catch (e) {
+                    return buildResponse(Errors.badImplementation('Error when JSON serializing the result of the extension point'));
+                }
             }
         });
     });
--- test/api.test.js
+++ test/api.test.js
@@ -23,6 +23,31 @@ describe('credentials-exchange', function () {
         });
     });
 
+    it('success when scope is undefined (unauthenticated)', function (done) {
+        compilers['credentials-exchange']({
+            nodejsCompiler,
+            script: 'module.exports = function(client, scope, audience, cb) { client.baz = "baz"; cb(null, { client, scope, audience }); };'
+        }, function (error, func) {
+            assert.ifError(error);
+            func({
+                body: { client: { id: 'client' }, audience: 'audience' },
+                headers: {}
+            }, function (error, data) {
+                assert.ifError(error);
+                assert.ok(data);
+                assert.equal(typeof data, 'object');
+                assert.equal(typeof data.client, 'object');
+                assert.equal(data.client.id, 'client');
+                assert.equal(data.client.baz, 'baz');
+                assert.equal(Object.keys(data.client).length, 2);
+                assert.equal(typeof data.scope, 'undefined');
+                assert.equal(data.audience, 'audience');
+                assert.equal(Object.keys(data).length, 3);
+                done();
+            });
+        });
+    });
+
     it('success getting, modifying, and returning body (unauthenticated)', function (done) {
         compilers['credentials-exchange']({
             nodejsCompiler,
@@ -79,10 +104,82 @@ describe('credentials-exchange', function () {
         });
     });
 
+    it('rejects calls with invalid payload', function (done) {
+        compilers['credentials-exchange']({
+            nodejsCompiler,
+            script: 'module.exports = function(client, scope, audience, cb) { cb(null, { client, scope, audience }); };'
+        }, function (error, func) {
+            assert.ifError(error);
+            func({
+                body: 'no good',
+                headers: {}
+            }, function (error, data) {
+                assert.ok(error);
+                assert.equal(error.statusCode, 400);
+                assert.equal(data, undefined);
+                done();
+            });
+        });
+    });
+
+    it('rejects calls with invalid payload (bad client)', function (done) {
+        compilers['credentials-exchange']({
+            nodejsCompiler,
+            script: 'module.exports = function(client, scope, audience, cb) { cb(null, { client, scope, audience }); };'
+        }, function (error, func) {
+            assert.ifError(error);
+            func({
+                body: { client: 'client', audience: 'audience' },
+                headers: {}
+            }, function (error, data) {
+                assert.ok(error);
+                assert.equal(error.statusCode, 400);
+                assert.equal(data, undefined);
+                done();
+            });
+        });
+    });
+
+    it('rejects calls with invalid payload (bad scope)', function (done) {
+        compilers['credentials-exchange']({
+            nodejsCompiler,
+            script: 'module.exports = function(client, scope, audience, cb) { cb(null, { client, scope, audience }); };'
+        }, function (error, func) {
+            assert.ifError(error);
+            func({
+                body: { client: {}, scope: 'scope', audience: 'audience' },
+                headers: {}
+            }, function (error, data) {
+                assert.ok(error);
+                assert.equal(error.statusCode, 400);
+                assert.equal(data, undefined);
+                done();
+            });
+        });
+    });
+
+    it('rejects calls with invalid payload (bad audience)', function (done) {
+        compilers['credentials-exchange']({
+            nodejsCompiler,
+            script: 'module.exports = function(client, scope, audience, cb) { cb(null, { client, scope, audience }); };'
+        }, function (error, func) {
+            assert.ifError(error);
+            func({
+                body: { client: {}, scope: 'scope', audience: [] },
+                headers: {}
+            }, function (error, data) {
+                assert.ok(error);
+                assert.equal(error.statusCode, 400);
+                assert.equal(data, undefined);
+                done();
+            });
+        });
+    });
+
     it('rejects calls without authorization secret', function (done) {
         compilers['credentials-exchange']({
             nodejsCompiler,
-            script: 'module.exports = function(client, scope, audience, cb) { ctx.baz = "baz"; cb(null, ctx); };'
+            script: 'module.exports = function(client, scope, audience, cb) { cb(null, { client, scope, audience }); };'
         }, function (error, func) {
             assert.ifError(error);
             func({
@@ -101,7 +198,7 @@ describe('credentials-exchange', function () {
     it('rejects calls with wrong authorization secret', function (done) {
         compilers['credentials-exchange']({
             nodejsCompiler,
-            script: 'module.exports = function(client, scope, audience, cb) { ctx.baz = "baz"; cb(null, ctx); };'
+            script: 'module.exports = function(client, scope, audience, cb) { cb(null, { client, scope, audience }); };'
         }, function (error, func) {
             assert.ifError(error);
             func({
