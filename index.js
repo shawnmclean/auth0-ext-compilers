a334c652ed8f0dfb2b1928573c20bd51b74bba50
Geoffrey Goodman <ggoodman@gmail.com>
2016-08-17 16:48:05 -0400

    Extract client_credentials_exchange and allow for output marshalling

--- index.js
+++ index.js
@@ -1,47 +1,6 @@
 'use strict';
 
-const Adapter = require('./lib/adapter');
-const Errors = require('./lib/errors');
-
 
 module.exports = {
-    'client-credentials-exchange': authorize_and_process_body,
+    'client-credentials-exchange': require('./lib/client_credentials_exchange'),
 };
-
-
-function authorize_and_process_body(options, cb) {
-    options.nodejsCompiler(options.script, function (error, func) {
-        if (error) return cb(error);
-        
-        
-        return cb(null, Adapter.wrap(handler));
-        
-        
-        function handler (ctx, cb) {
-            if (is_authorized(ctx, cb)) {
-                if (typeof ctx.body !== 'object')
-                    return cb(Errors.badRequest());
-                if (typeof ctx.body.client !== 'object')
-                    return cb(Errors.badRequest());
-                if (ctx.body.scope && !Array.isArray(ctx.body.scope))
-                    return cb(Errors.badRequest());
-                if (typeof ctx.body.audience !== 'string')
-                    return cb(Errors.badRequest());
-
-                return func(ctx.body.client, ctx.body.scope, ctx.body.audience, cb);
-            }
-
-        }
-    });
-}
-
-function is_authorized(ctx, cb) {
-    if (ctx.secrets && ctx.secrets['auth0-extension-secret']) {
-        // Authorization is required, enforce
-        var match = (ctx.headers['authorization'] || '').trim().match(/^bearer (.+)$/i);
-        if (match && match[1] === ctx.secrets['auth0-extension-secret']) return true;
-        cb(Errors.unauthorized());
-        return false;
-    }
-    return true;
-}
